79c1005497f155f5030068e94e3805ed0b63072b
Holger Schurig <holgerschurig@gmail.com>
2009-10-23 12:33:37 +0200

    eproject: tweaks to libertas-project

--- init.el
+++ init.el
@@ -1729,12 +1729,21 @@ Otherwise, kill characters backward until encountering the end of a word."
 (flet ((look-for (expr &optional (expr-type :filename)) )))
 
 (define-project-type libertas (generic)
-  (look-for "persistcfg.c"))
+  (look-for "persistcfg.c")
+  :relevant-files ("\\*.c$" "\\.h" "Makefile" "Kconfig"))
 
 (add-hook 'libertas-project-file-visit-hook
           (lambda ()
             (set (make-local-variable 'compile-command)
                  "make -C /usr/src/linux-wl SUBDIRS=drivers/net/wireless/libertas")))
+
+;; (define-project-type lisp (generic)
+;;   (eproject--scan-parents-for file
+;; 			      (lambda (directory)
+;; 				(let ((dname (file-name-nondirectory directory)))
+;; 				  (file-exists-p (format "%s/%s.asd" directory dname)))))
+;;   :relevant-files ("\\.lisp$" "\\.asd$"))
+
 
 ;;}}}
 ;;{{{ Package: erc
