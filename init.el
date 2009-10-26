855772bfbc5a86e9e6f1f044ea5889ad618e00e8
Holger Schurig <holgerschurig@gmail.com>
2009-10-26 09:30:56 +0100

    byte-code-cache: use eval-after-load

--- init.el
+++ init.el
@@ -1,9 +1,10 @@
 ;; Minimal startup file, the core is in start.el
 
+(eval-after-load "byte-code-cache"
+  '(setq bcc-cache-directory "~/.emacs.d/tmp/byte-cache"
+	 bcc-blacklist '("/recentf\\.el$" "/history\\.el$")
+	 byte-compile-warnings t
+	 byte-compile-verbose nil))
 (load (expand-file-name "~/.emacs.d/elisp/byte-code-cache.el"))
-(setq bcc-cache-directory "~/.emacs.d/tmp/byte-cache"
-      bcc-blacklist '("/recentf\\.el$" "/history\\.el$")
-      byte-compile-warnings t
-      byte-compile-verbose t)
 
 (load (expand-file-name "~/.emacs.d/start.el"))
