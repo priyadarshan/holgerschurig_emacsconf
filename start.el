cfb7ea95f53ba804e92ba3255a0e90ce22ca1c1a
Holger Schurig <holgerschurig@gmail.com>
2010-03-31 09:31:22 +0200

    ido: sort files by modification

--- start.el
+++ start.el
@@ -1979,6 +1979,24 @@ Otherwise, kill characters backward until encountering the end of a word."
 ;;       (all-completions "" obarray 'commandp))))))
 
 
+;; sort ido filelist by mtime instead of alphabetically
+;; http://www.emacswiki.org/emacs/InteractivelyDoThings
+(defun ido-sort-mtime ()
+  (setq ido-temp-list
+	(sort ido-temp-list
+	      (lambda (a b)
+		(let ((ta (nth 5 (file-attributes (concat ido-current-directory a))))
+		      (tb (nth 5 (file-attributes (concat ido-current-directory b)))))
+		  (if (= (nth 0 ta) (nth 0 tb))
+		      (> (nth 1 ta) (nth 1 tb))
+		    (> (nth 0 ta) (nth 0 tb)))))))
+  (ido-to-end  ;; move . files to end (again)
+   (delq nil (mapcar
+	      (lambda (x) (if (and (not (string-equal x ".")) (string-equal (substring x 0 1) ".")) x))
+	      ido-temp-list))))
+(add-hook 'ido-make-file-list-hook 'ido-sort-mtime)
+(add-hook 'ido-make-dir-list-hook 'ido-sort-mtime)
+
 
 ;;}}}
 ;;{{{ Package: magit
