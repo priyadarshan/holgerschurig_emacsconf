277696e143ff84dbabb46cf0ebc7401ae7a69ce1
Holger Schurig <holgerschurig@gmail.com>
2012-08-22 16:17:53 +0200

    completion: ignore some file extensions used by LaTeX / latexmk

--- start.el
+++ start.el
@@ -2248,7 +2248,15 @@ Otherwise, kill characters backward until encountering the end of a word."
 	   "\\`\\.\\./"
 	   "\\`\\./")
 	 ))
-
+;; Ignore some files from latex / latexmk
+(add-to-list 'completion-ignored-extensions ".aux")
+(add-to-list 'completion-ignored-extensions ".dvi")
+(add-to-list 'completion-ignored-extensions ".fdb_latexmk")
+(add-to-list 'completion-ignored-extensions ".idx")
+(add-to-list 'completion-ignored-extensions ".ilg")
+(add-to-list 'completion-ignored-extensions ".ind")
+(add-to-list 'completion-ignored-extensions ".pdf")
+(add-to-list 'completion-ignored-extensions ".toc")
 (ido-mode 'both)
 
 
