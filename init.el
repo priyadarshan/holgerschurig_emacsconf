1525dfcbc0e1d11b3713c2ae60969d57c41d96da
Holger Schurig <holgerschurig@gmail.com>
2013-10-15 15:30:10 +0200

    org: specify list of loaded org-modules early
    Otherwise tangling of emacs.org would load all of those files ...

--- init.el
+++ init.el
@@ -13,6 +13,25 @@
 (load (concat dotfiles-dir "elisp/byte-code-cache.el"))
 
 
+;; Some values must be set before emacs.org auto-loads org.el ...
+(setq ;; Only load these org modules:
+      org-modules (;; 'org-bbdb
+		   ;; 'org-bibtex
+		   ;; 'org-docview
+		   ;; 'org-gnus
+		   ;; 'org-info
+		   ;; 'org-jsinfo
+		   ;; 'org-irc
+		   ;; 'org-mew
+		   ;; 'org-mhe
+		   ;; 'org-rmail
+		   ;; 'org-vm
+		   ;; 'org-w3m
+		   ;; 'org-wl
+		   )
+      org-replace-disputed-keys t)
+
+
 (add-to-list 'load-path (concat (mapconcat 'identity (file-expand-wildcards (concat dotfiles-dir "elpa/org-20*")) "") "/"))
 (require 'ob-tangle)
 (defun load-org-elisp (file &optional compile)
