bae219e75da22e2c224c93de883c671f50f08e2b
Holger Schurig <holgerschurig@gmail.com>
2015-01-11 01:26:16 +0100

    Display: add afternoon theme

--- custom.el
+++ custom.el
@@ -3,16 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((folding-internal-margins) (folded-file . t)))))
+)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(diff-file-header ((t nil)))
- '(diff-header ((t nil)))
- '(font-lock-constant-face ((((class color) (min-colors 88) (background light)) (:foreground "SlateBlue4"))))
- '(font-lock-keyword-face ((t (:weight bold))))
- '(font-lock-string-face ((((class color) (min-colors 88) (background light)) (:foreground "Forest Green"))))
- '(magit-item-highlight ((t nil)))
- '(region ((t (:background "gold" :foreground "gtk_selection_fg_color")))))
+ )
