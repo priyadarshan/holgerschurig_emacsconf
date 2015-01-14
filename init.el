de56d2af0b5d5015f1464e1a2a35ee99ceb6ce08
Holger Schurig <holgerschurig@gmail.com>
2015-01-14 08:43:07 +0100

    flycheck: specify checkers

--- init.el
+++ init.el
@@ -1665,6 +1665,51 @@ If the CDR is nil, then the buffer is only buried."
   :commands flycheck-mode
   :init
   (progn
+    (setq flycheck-checkers '(;; ada-gnat
+			      ;; asciidoc
+			      c/c++-gcc ;; c/c++-clang c/c++-cppcheck
+			      ;; cfengine
+			      ;; chef-foodcritic
+			      ;; coffee coffee-coffeelint
+			      ;; coq
+			      css-csslint
+			      ;; d-dmd
+			      ;; elixir
+			      emacs-lisp ;; emacs-lisp-checkdoc
+			      ;; erlang
+			      ;; eruby-erubis
+			      ;; fortran-gfortran
+			      ;; go-gofmt go-golint go-vet go-build go-test go-errcheck
+			      ;; haml
+			      ;; handlebars
+			      ;; haskell-ghc haskell-hlint
+			      html-tidy
+			      ;; javascript-jshint javascript-eslint javascript-gjslint
+			      json-jsonlint
+			      ;; less
+			      ;; lua
+			      make
+			      ;; perl perl-perlcritic
+			      ;; php php-phpmd php-phpcs
+			      ;; puppet-parser puppet-lint
+			      python-flake8 python-pylint python-pycompile
+			      ;; r-lintr
+			      ;; racket
+			      ;; rpm-rpmlint
+			      rst rst-sphinx
+			      ;; ruby-rubocop ruby-rubylint ruby ruby-jruby
+			      ;; rust
+			      ;; sass
+			      ;; scala scala-scalastyle
+			      ;; scss
+			      sh-bash sh-posix-dash sh-posix-bash sh-zsh sh-shellcheck
+			      ;; slim
+			      ;; tex-chktex tex-lacheck
+			      ;; texinfo
+			      ;; verilog-verilator
+			      ;; xml-xmlstarlet xml-xmllint
+			      ;; yaml-jsyaml yaml-ruby
+			      ))
     (add-hook 'prog-mode-hook 'my--start-flycheck)
   ))
 
