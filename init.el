fdd2800b1ec4e395114579e16d4942eac7a97b8c
Holger Schurig <holgerschurig@gmail.com>
2015-08-10 09:02:06 +0200

    iflipb: remove some warnings

--- init.el
+++ init.el
@@ -680,8 +680,6 @@ If the CDR is nil, then the buffer is only buried."
 (use-package iflipb
   :defer t
   :commands (iflipb-next-buffer iflipb-previous-buffer)
-  :bind (("<f6>"   . my-iflipb-next-buffer)
-  	 ("S-<f6>" . my-iflipb-previous-buffer))
   :config
   (csetq iflipb-wrap-around t)
 
@@ -699,6 +697,7 @@ If the CDR is nil, then the buffer is only buried."
 	(cancel-timer my-iflipb-auto-off-timer-canceler-internal))
     (run-with-idle-timer my-iflipb-auto-off-timeout-sec 0 'my-iflipb-auto-off)
     (setq my-iflipb-ing-internal t))
+  (bind-key "<f6>" 'my-iflipb-next-buffer)
   (defun my-iflipb-previous-buffer ()
     (interactive)
     (iflipb-previous-buffer)
@@ -706,13 +705,13 @@ If the CDR is nil, then the buffer is only buried."
 	(cancel-timer my-iflipb-auto-off-timer-canceler-internal))
     (run-with-idle-timer my-iflipb-auto-off-timeout-sec 0 'my-iflipb-auto-off)
     (setq my-iflipb-ing-internal t))
+  (bind-key "S-<f6>" 'my-iflipb-previous-buffer)
   (defun iflipb-first-iflipb-buffer-switch-command ()
     "Determines whether this is the first invocation of
   iflipb-next-buffer or iflipb-previous-buffer this round."
     (not (and (or (eq last-command 'my-iflipb-next-buffer)
 		  (eq last-command 'my-iflipb-previous-buffer))
-	      my-iflipb-ing-internal)))
-)
+	      my-iflipb-ing-internal))))
 ;;;_  . ace-jump-buffer
 (use-package ace-jump-buffer
   :defer t
