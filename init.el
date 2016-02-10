fc15cfa6374ac2d132b3da939991ee914bff341d
Holger Schurig <holgerschurig@gmail.com>
2016-02-10 10:31:18 +0100

    init.el: remove superfluous load-paths

--- init.el
+++ init.el
@@ -5,10 +5,6 @@
 ;; Override the packages with the git version of Org and other packages
 (add-to-list 'load-path (concat user-emacs-directory "elisp"))
 (add-to-list 'load-path (concat user-emacs-directory "elisp/org-mode/lisp"))
-(add-to-list 'load-path (concat user-emacs-directory "org-mode/contrib/lisp"))
-;;(add-to-list 'load-path "~/code/org2blog")
-;;(add-to-list 'load-path "~/Dropbox/2014/presentations/org-reveal")
-;;(add-to-list 'load-path "/usr/local/share/emacs/site-lisp")
 ;; Load the rest of the packages
 (package-initialize nil)
 (setq package-enable-at-startup nil)
