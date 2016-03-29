6894fb83daf4ee634f3663bbbb0a1b2f5d1e193a
Holger Schurig <holgerschurig@gmail.com>
2016-03-29 23:19:10 +0200

    init.el: reformatting

--- init.el
+++ init.el
@@ -20,30 +20,30 @@
 - have a source-code of =emacs-lisp="
   (require 'org)
   (let* ((body-list ())
-	 (output-file "config.el")
-	 (org-babel-default-header-args (org-babel-merge-params org-babel-default-header-args
-								  (list (cons :tangle output-file)))))
+         (output-file "config.el")
+         (org-babel-default-header-args (org-babel-merge-params org-babel-default-header-args
+                                                                (list (cons :tangle output-file)))))
     (message "Writing %s ..." output-file)
     (save-restriction
       (save-excursion
-		(org-babel-map-src-blocks "config.org"
-		  (let* ((info (org-babel-get-src-block-info 'light))
-				 (tfile (cdr (assq :tangle (nth 2 info))))
-				 (match))
-			(save-excursion
-			  (catch 'exit
-				(org-back-to-heading t)
-				(when (looking-at org-outline-regexp)
-				  (goto-char (1- (match-end 0))))
-				(when (looking-at (concat " +" org-todo-regexp "\\( +\\|[ \t]*$\\)"))
-				  (setq match (match-string 1)))))
-			(unless (or (string= "no" tfile)
-						(string= "CANCELED" match)
-						(not (string= "emacs-lisp" lang)))
-			  (add-to-list 'body-list body)))))
+        (org-babel-map-src-blocks "config.org"
+                                  (let* ((info (org-babel-get-src-block-info 'light))
+                                         (tfile (cdr (assq :tangle (nth 2 info))))
+                                         (match))
+                                    (save-excursion
+                                      (catch 'exit
+                                        (org-back-to-heading t)
+                                        (when (looking-at org-outline-regexp)
+                                          (goto-char (1- (match-end 0))))
+                                        (when (looking-at (concat " +" org-todo-regexp "\\( +\\|[ \t]*$\\)"))
+                                          (setq match (match-string 1)))))
+                                    (unless (or (string= "no" tfile)
+                                                (string= "CANCELED" match)
+                                                (not (string= "emacs-lisp" lang)))
+                                      (add-to-list 'body-list body)))))
       (with-temp-file output-file
-		(insert ";; Don't edit this file, edit config.org' instead ...\n\n")
-		(insert (apply 'concat (reverse body-list))))
+        (insert ";; Don't edit this file, edit config.org' instead ...\n\n")
+        (insert (apply 'concat (reverse body-list))))
       (message "Wrote %s ..." output-file))))
 
 (let ((el-file (concat user-emacs-directory "config.el"))
