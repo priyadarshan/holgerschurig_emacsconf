c23cadbb5062997acdd0452d123e9b29d1407ad1
Holger Schurig <holgerschurig@gmail.com>
2016-11-10 10:29:19 +0100

    git-timemachine: enable

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-	(magit zeal-at-point rainbow-mode dumb-jump column-marker web-mode pug-mode markdown-mode lua-mode js2-mode go-mode d-mode dtrt-indent clean-aindent-mode pdf-tools circe htmlize helm-swoop helm-descbinds helm which-key iflipb avy-zap smartscan avy expand-region undo-tree keyfreq dash powerline paradox use-package))))
+	(git-timemachine magit zeal-at-point rainbow-mode dumb-jump column-marker web-mode pug-mode markdown-mode lua-mode js2-mode go-mode d-mode dtrt-indent clean-aindent-mode pdf-tools circe htmlize helm-swoop helm-descbinds helm which-key iflipb avy-zap smartscan avy expand-region undo-tree keyfreq dash powerline paradox use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
